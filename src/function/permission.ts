9604b3d8af3ab188c359d86945f30645093a5a9d
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-12-13 00:54:24 +0900

    관리자 유저 추가

--- src/function/permission.ts
+++ src/function/permission.ts
@@ -2,6 +2,8 @@ import { client } from "..";
 import MDB from "../database/Mongodb";
 import { I, M } from "../aliases/discord.js";
 import { GuildMemberRoleManager, MessageEmbed, Permissions } from "discord.js";
+import { config } from "dotenv";
+config();
 
 /**
  * DB
@@ -9,12 +11,13 @@ import { GuildMemberRoleManager, MessageEmbed, Permissions } from "discord.js";
  */
 
 export async function check_permission(msg: I | M): Promise<boolean> {
-  let userper = msg.member?.permissions;
-  if (permissonstype(userper)) if (userper.has('ADMINISTRATOR')) return true;
+  if (process.env.ADMINID && (process.env.ADMINID === msg.member?.user.id)) return true;
+  let userper = msg.member?.permissions as Permissions;
+  if (userper) if (userper.has('ADMINISTRATOR')) return true;
   let guildDB = await MDB.get.guild(msg);
   let guildrole = guildDB!.role;
-  let userrole = msg.member?.roles;
-  if (rolestype(userrole)) if (userrole.cache.some((role) => guildrole.includes(role.id))) return true;
+  let userrole = msg.member?.roles as GuildMemberRoleManager;
+  if (userrole) if (userrole.cache.some((role) => guildrole.includes(role.id))) return true;
   return false;
 }
 
@@ -22,10 +25,3 @@ export const embed_permission: MessageEmbed = client.mkembed({
   description: `이 명령어를 사용할\n권한이 없습니다.`,
   color: 'DARK_RED'
 });
-
-function permissonstype(permisson: string | Permissions | undefined): permisson is Permissions {
-  return (<Permissions>permisson) !== undefined;
-}
-function rolestype(roles: GuildMemberRoleManager | string[] | undefined): roles is GuildMemberRoleManager {
-  return (<GuildMemberRoleManager>roles) !== undefined;
-}
