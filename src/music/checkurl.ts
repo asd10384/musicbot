45925fc327f4cd8e39a342d3975894b7ea2d5590
tkrmagid <tmdgks0466@naver.com>
2022-10-31 23:02:11 +0900

    빌보두 리스트 삭제

--- src/music/checkurl.ts
+++ src/music/checkurl.ts
@@ -2,12 +2,10 @@
 export default function checkurl(text: string) {
   const checkvideo = /^(?:https?:\/\/)?(?:m\.|www\.|music\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
   const checklist = /^(?:https?:\/\/)?(?:m\.|www\.|music\.)?(?:youtu\.be\/|youtube\.com\/(?:playlist\?list=))((\w|-).+)(?:\S+)?$/;
-  const checkbillboardoo = /^(?:https?:\/\/)wakmusic\.xyz\/player\/((\w|-){11})/;
-  const checkbillboardoolist = /^(?:https?:\/\/)wakmusic\.xyz\/player\/((\w|-){11})\?prev=\&next=/;
+  const checkbillboardoo = /^(?:https?:\/\/)wakmusic\.xyz\/player\/((\w|-){11}).+/;
   return {
     video: text.match(checkvideo),
     list: text.match(checklist),
-    billboardoo: text.match(checkbillboardoo),
-    billboardoolist: text.match(checkbillboardoolist)
+    billboardoo: text.match(checkbillboardoo)
   };
 }
